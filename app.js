903b8953d9b5321270825bfa8a6e4bb2079a5469
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-20 21:26:15 +0600

    let concept explored

---
+++ app.js
@@ -0,0 +1,34 @@
+// 1. what is ES6 and why we should use it?
+    /*
+     ==> ES6 is the updated version of javascript
+     ==> increase readability
+     ==> short code
+    
+    */
+
+// 2. let
+//===============================
+let myName = "Programming Hero";
+// let myName = 'kamrul';  // ==> we can not redeclare with same variable name with let keyword
+myName = 'changed value';  // ==> we can change only the value 
+
+
+// ==> ReferenceError: Cannot access 'age' before initialization whe variable declared with let keyword;
+/* age = 10;    
+let age;
+console.log(age) */
+
+
+
+// block scope
+let x = 10
+{
+    let x = 2;    // block scope
+    console.log('inside block' , x);
+}
+console.log('outside block', x);
+
+
+
+// 3. const
+//====================================
